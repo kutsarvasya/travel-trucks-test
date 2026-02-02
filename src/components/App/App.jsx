90b55aa50d9fa32c7d1510d063c7c547f919bcf2
Vasyl Kutsar <vkutsar89@gmail.com>
2026-02-02 01:09:05 +0100

    add lazy and suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,20 @@
+import { lazy, Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
-import HomePage from "../../pages/HomePage/HomePage";
-import CatalogPage from "../../pages/CatalogPage/CatalogPage";
-import CamperDetailsPage from "../../pages/CamperDetailsPage/CamperDetailsPage";
-import Features from "../Features/Features";
-import Reviews from "../Reviews/Reviews";
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("../../pages/CatalogPage/CatalogPage"));
+const CamperDetailsPage = lazy(
+  () => import("../../pages/CamperDetailsPage/CamperDetailsPage"),
+);
+const Features = lazy(() => import("../../components/Features/Features"));
+const Reviews = lazy(() => import("../../components/Reviews/Reviews"));
+const NotFoundPage = lazy(
+  () => import("../../pages/NotFoundPage/NotFoundPage"),
+);
 import { selectError, selectLoading } from "../../redux/catalog/slice";
 import { useSelector } from "react-redux";
 import Loader from "../Loader/Loader";
 import { ToastContainer, toast } from "react-toastify";
-import { useEffect } from "react";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 function App() {
   const isLoading = useSelector(selectLoading);
@@ -22,17 +26,19 @@ function App() {
   }, [isError]);
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route index element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CamperDetailsPage />}>
-            <Route index element={<Features />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<HomePage />} />
+            <Route path="catalog" element={<CatalogPage />} />
+            <Route path="catalog/:id" element={<CamperDetailsPage />}>
+              <Route index element={<Features />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
       {isLoading && <Loader />}
       <ToastContainer position="top-right" autoClose={3000} />
     </div>
